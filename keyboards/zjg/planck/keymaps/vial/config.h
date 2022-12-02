8d95576e696f20177e79b818efa9b6e779619357
Doug Rosvick <drosvick@gmail.com>
2022-12-01 21:39:13 -0700

    add vial keymap for zjg/planck

---
+++ keyboards/zjg/planck/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE4, 0xDE, 0xCB, 0xFE, 0xA6, 0x65, 0xA6, 0x1D}
+#define VIAL_UNLOCK_COMBO_ROWS {1, 0}
+#define VIAL_UNLOCK_COMBO_COLS {2, 3}
